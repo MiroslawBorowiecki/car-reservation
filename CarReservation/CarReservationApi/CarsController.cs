0469aff24207ff00f7fe59a63b8ebe6b9585aa58
Mirosław Borowiecki <mborowiecki@graylight-imaging.com>
2023-07-18 21:10:48 +0200

    First test and its simplest implementation

---
+++ CarReservation/CarReservationApi/CarsController.cs
@@ -0,0 +1,14 @@
+﻿namespace CarReservationApi;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class CarsController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Add()
+    {
+        return BadRequest();
+    }
+}
---
+++ CarReservation/CarReservationApiTests/CarTests.cs
@@ -0,0 +1,25 @@
+﻿namespace CarReservationApi.Tests;
+
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+[TestClass]
+public class CarTests
+{
+    private WebApplicationFactory<Program> _factory = new();
+
+    [TestMethod]
+    public async Task GivenNoDataIsProvided_WhenITryToAddACar()
+    {
+        HttpClient client = _factory.CreateClient();
+
+        var response = await client.PostAsync("/cars", null);
+
+        ItDoesNotAddACar(response);
+    }
+
+    private static void ItDoesNotAddACar(HttpResponseMessage response)
+    {
+        Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}
